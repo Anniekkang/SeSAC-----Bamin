60f5ef5e0926b209c33183a5f22243001cc07849
annie <knr9144@icloud.com>
2022-07-08 16:14:53 +0900

    Initial Commit

---
+++ DeliveryApp/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  DeliveryApp
+//
+//  Created by 나리강 on 2022/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var noodleStackView: UIStackView!
+    @IBOutlet weak var chickenStackView: UIStackView!
+    @IBOutlet weak var breadStackView: UIStackView!
+    @IBOutlet weak var hanburgerStackView: UIStackView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    print(1)
+    print(2)
+        
+        
+        
+       
+    }
+
+
+}
+
